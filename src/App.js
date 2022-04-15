448bf86b4e977950837545b15569bbf1b9b2f94d
mickeycoi <datnguyenthanh1410@gmail.com>
2022-04-16 00:02:41 +0700

    final timer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
 
 function App() {
-  const { time, startTimer, stopTimer, resetTimer, splitTimer, active } =
+  const { time, startTimer, stopTimer, resetTimer, splitTimer, active, times } =
     useTimer(0);
 
   return (
     <div className="App container">
       <h1>Coder Timer</h1>
       <div className="timer__wrapper">
+        <div className="slpit-timer">
+          {times.map((time, index) => (
+            <div className="timer__display" key={index}>
+              <p
+                style={{
+                  fontSize: "2.5rem",
+                  paddingTop: "10px",
+                }}
+              >
+                {index < 9
+                  ? `0${index + 1}. ${formatTime(time)}`
+                  : `${index + 1}. ${formatTime(time)}`}
+              </p>
+            </div>
+          ))}
+        </div>
         <div className="timer__display">
           <p>{formatTime(time)}</p>
         </div>
--- src/useTimer.js
+++ src/useTimer.js
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
   const [time, setTime] = useState(0);
+  const [times, setTimes] = useState([]);
 
   const isStart = useRef(true);
   const active = useRef();
@@ -19,14 +20,20 @@ const useTimer = (ini = 0) => {
   const stopTimer = () => {
     isStart.current = false;
     clearInterval(refInterval.current);
+    active.current.disabled = false;
   };
   const resetTimer = () => {
     setTime(0);
+    setTimes([]);
     clearInterval(refInterval.current);
     active.current.disabled = false;
   };
-  const splitTimer = (e) => {};
+  const splitTimer = () => {
+    if (isStart.current) {
+      setTimes([...times, time]);
+    }
+  };
 
-  return { time, startTimer, stopTimer, resetTimer, splitTimer, active };
+  return { time, startTimer, stopTimer, resetTimer, splitTimer, active, times };
 };
 export default useTimer;
